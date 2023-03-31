b761eee43387616263f65ea448ee45f451322784
daniitoro29 <daniitoro29@gmail.com>
2023-03-31 06:37:36 -0500

    Start form

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,14 +1,5 @@
 import axios from "axios";
 
-/* export const getUsers = () => {
-    return async function(dispatch){
-        // Hace la petición
-        const apiData = await axios.get("http://localhost:3001/users");
-        const users = apiData.data;
-        // Despacha la acción
-        dispatch({type: GET_USERS, payload:users});
-    };
-} */
 export const GET_USERS = 'GET_USERS';
 
 export const getUsers = () => async (dispatch) => {
--- client/src/views/Form/Form.jsx
+++ client/src/views/Form/Form.jsx
@@ -1,37 +1,64 @@
+import { useState } from "react";
+
 const Form = () => {
+  const [form, setForm] = useState({
+    nombre: "",
+    apellido: "",
+    telefono: "",
+    correo: "",
+    contraseña: "",
+    genero: "",
+    estado: "",
+  });
+
+const changeHandler = (event) => {
+  const property = event.target.name;
+  const value = event.target.value;
+
+  setForm({
+    ...form, [property]:value
+  })
+}
+
   return (
     <>
-    <h2>Registrarse</h2>
+      <h2>Registrarse</h2>
       <form>
-        <label>
-          Nombre:
-          <input type="text" name="nombre" />
-        </label>
-        <label>
-          Apellido:
-          <input type="text" name="apellido" />
-        </label>
-        <label>
-          Teléfono:
-          <input type="text" name="telefono" />
-        </label>
-        <label>
-          Correo:
-          <input type="text" name="correo" />
-        </label>
-        <label>
-          Contraseña:
-          <input type="text" name="contraseña" />
-        </label>
-        <label>
-          Género:
-          <input type="text" name="genero" />
-        </label>
-        <label>
-          Estado:
-          <input type="text" name="estado" />
-        </label>
-        <input type="submit" value="Submit" />
+        <div>
+          <label>Nombre: </label>
+          <input type="text" name="nombre" value={form.nombre} onChange={changeHandler}/>
+        </div>
+
+        <div>
+          <label>Apellido: </label>
+          <input type="text" name="apellido" value={form.apellido} onChange={changeHandler}/>
+        </div>
+
+        <div>
+          <label>Teléfono: </label>
+          <input type="text" name="telefono" value={form.telefono} onChange={changeHandler}/>
+        </div>
+
+        <div>
+          <label>Correo: </label>
+          <input type="text" name="correo" value={form.correo} onChange={changeHandler}/>
+        </div>
+
+        <div>
+          <label>Contraseña: </label>
+          <input type="text" name="contraseña" value={form.contraseña} onChange={changeHandler}/>
+        </div>
+
+        <div>
+          <label>Género: </label>
+          <input type="text" name="genero" value={form.genero} onChange={changeHandler}/>
+        </div>
+
+        <div>
+          <label>Estado: </label>
+          <input type="text" name="estado" value={form.estado} onChange={changeHandler}/>
+        </div>
+        <input type="submit" value="Registrarse" />
       </form>
     </>
   );
